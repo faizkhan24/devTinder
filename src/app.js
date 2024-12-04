fac427c256d5849c83ed3aa0b4ea5d2a3f7b329a
faizkhan <khanfaiz33897@gmail.com>
2024-12-04 21:57:41 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send('Home')
+// This will only handle GET call to /user
+
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Faiz", lastName: "Khan" })
 })
 
-app.get("/hello",(req,res)=>{
-    res.send('Hello... Hello... Hello...');
+app.post("/user", (req,res)=>{
+    // console.log("Save Data to the database");
+    res.send('Data successfully saved to the database')
+    
 })
 
-app.get("/test",(req,res)=>{
+app.delete("/user", (req,res)=>{
+    res.send("Deleted Succesfully")
+})
+// this will match all the HTTP method API calls to /test
+app.use("/test",(req,res)=>{
     res.send('Hello from the server')
 })
+
+
 
 app.listen(3000, ()=>{
     console.log('Server is successfully listening on port 3000...');
